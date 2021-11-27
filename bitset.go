7830b46452abc2f27ec697589c6752dc4f330ba9
erp <erp@wildberries.ru>
2021-11-27 16:24:57 +0300

    * BitsToBites

--- bitset.go
+++ bitset.go
@@ -1,5 +1,7 @@
 package vector
 
+import "math/bits"
+
 var bitsTable [256]uint64
 
 func init() {
@@ -16,4 +18,41 @@ func bytesToBits(r *[4]uint64, b []uint8) {
 	for i := 0; i < len(b); i++ {
 		r[(b[i] >> 6)] |= bitsTable[b[i]]
 	}
+}
+
+func bitsToBytesDump(r [4]uint64, b []uint8) []uint8 {
+	b = b[:0]
+	for i := 0; i < 4; i++ {
+		if r[i] == 0 {
+			continue
+		}
+		off := uint8(i * 64)
+		for x := 0; x < 64; x++ {
+			if r[i]&(1<<x) != 0 {
+				b = append(b, uint8(x)+off)
+			}
+		}
+	}
+	return b
+}
+
+func bitsToBytes(r [4]uint64, b []uint8) []uint8 {
+	n := 0
+	for i := 0; i < 4; i++ {
+		off := uint8(i << 6)
+		v := 0
+		rv := r[i]
+		for {
+			if rv == 0 {
+				break
+			}
+			x := bits.TrailingZeros64(rv)
+			b[n] = uint8(x+v) + off
+			n++
+			x++
+			v += x
+			rv >>= x
+		}
+	}
+	return b[:n]
 }
--- bitset_test.go
+++ bitset_test.go
@@ -9,3 +9,67 @@ func BenchmarkBytesToBits(b *testing.B) {
 		bytesToBits(r, buf)
 	}
 }
+
+func BenchmarkBitsToBitesDump_100K(b *testing.B) {
+	s := make([][4]uint64, 100000)
+	for i := 0; i < len(s); i++ {
+		s[i][0] = uint64(i)
+		s[i][1] = uint64(i)
+		s[i][2] = uint64(i)
+		s[i][3] = uint64(i)
+	}
+	for i := 0; i < b.N; i++ {
+		x := make([]byte, 256)
+		for k := 0; k < len(s); k++ {
+			_ = bitsToBytesDump(s[i], x)
+		}
+	}
+}
+
+func BenchmarkBitsToBites_100K(b *testing.B) {
+	s := make([][4]uint64, 100000)
+	for i := 0; i < len(s); i++ {
+		s[i][0] = uint64(i)
+		s[i][1] = uint64(i)
+		s[i][2] = uint64(i)
+		s[i][3] = uint64(i)
+	}
+	for i := 0; i < b.N; i++ {
+		x := make([]byte, 256)
+		for k := 0; k < len(s); k++ {
+			_ = bitsToBytes(s[i], x)
+		}
+	}
+}
+
+func BenchmarkBitsToBitesDump_500K(b *testing.B) {
+	s := make([][4]uint64, 500000)
+	for i := 0; i < len(s); i++ {
+		s[i][0] = uint64(i)
+		s[i][1] = uint64(i)
+		s[i][2] = uint64(i)
+		s[i][3] = uint64(i)
+	}
+	for i := 0; i < b.N; i++ {
+		x := make([]byte, 256)
+		for k := 0; k < len(s); k++ {
+			_ = bitsToBytesDump(s[i], x)
+		}
+	}
+}
+
+func BenchmarkBitsToBites_500K(b *testing.B) {
+	s := make([][4]uint64, 500000)
+	for i := 0; i < len(s); i++ {
+		s[i][0] = uint64(i)
+		s[i][1] = uint64(i)
+		s[i][2] = uint64(i)
+		s[i][3] = uint64(i)
+	}
+	for i := 0; i < b.N; i++ {
+		x := make([]byte, 256)
+		for k := 0; k < len(s); k++ {
+			_ = bitsToBytes(s[i], x)
+		}
+	}
+}
