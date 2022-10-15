88ff348f0200fc077c696e780f5ed28fb4b76775
freepk <freepk@mail.ru>
2022-10-15 22:02:28 +0300

    Cleanup

--- vector2.go
+++ vector2.go
@@ -3,7 +3,6 @@ package vector
 import "unsafe"
 
 type Vector2 struct {
-	//test int
 	last int
 	data []uint16
 }
@@ -13,7 +12,6 @@ func NewVector2() *Vector2 {
 }
 
 func (v *Vector2) Clear() {
-	//v.test = 0
 	v.last = 0
 	v.data = v.data[:0]
 }
@@ -28,24 +26,20 @@ func (v *Vector2) Add(n int) {
 	size := len(v.data)
 	base, mask, data := unpack(n)
 	if size == 0 {
-		//v.test = n
 		v.last = size
 		v.data = append(v.data, base, mask, data)
-	} else /* if n > v.test */ {
+	} else {
 		i := v.last
 		if base == v.data[i] {
 			i++
 			if mask > v.data[i] {
-				//v.test = n
 				v.data[i] |= mask
 				v.data = append(v.data, data)
 			} else {
 				i++
-				//v.test = n
 				v.data[i] |= data
 			}
 		} else {
-			//v.test = n
 			v.last = size
 			v.data = append(v.data, base, mask, data)
 		}
