5e33d01c25aabfe8ef0de3c4f7826a8b434f643c
freepk <freepk@mail.ru>
2022-10-16 00:05:17 +0300

    Vector2Iter

--- vector2.go
+++ vector2.go
@@ -1,6 +1,7 @@
 package vector
 
 import "unsafe"
+import "math/bits"
 
 type Vector2 struct {
 	last int
@@ -36,8 +37,7 @@ func (v *Vector2) Add(n int) {
 				v.data[i] |= mask
 				v.data = append(v.data, data)
 			} else {
-				i++
-				v.data[i] |= data
+				v.data[(size - 1)] |= data
 			}
 		} else {
 			v.last = size
@@ -50,4 +50,42 @@ func (v *Vector2) Bytes() []uint8 {
 	n := len(v.data) * 2
 	p := (*[0xffffffff]uint8)(unsafe.Pointer(&v.data[0]))
 	return p[:n]
+}
+
+type Vector2Iter struct {
+	pos int
+	vec *Vector2
+}
+
+func NewVector2Iter(v *Vector2) *Vector2Iter {
+	return &Vector2Iter{vec: v}
+}
+
+func (vi *Vector2Iter) Reset() {
+	vi.pos = 0
+}
+
+func (vi *Vector2Iter) hasNext() bool {
+	if len(vi.vec.data) == 0 {
+		return false
+	}
+	if vi.pos > vi.vec.last {
+		return false
+	}
+	return true
+}
+
+func (vi *Vector2Iter) Next() (base uint16, mask uint16, data []uint16, ok bool) {
+	if !vi.hasNext() {
+		return
+	}
+	i := vi.pos
+	base = vi.vec.data[i]
+	i++
+	mask = vi.vec.data[i]
+	i++
+	vi.pos = i + bits.OnesCount16(mask)
+	data = vi.vec.data[i:vi.pos]
+	ok = true
+	return
 }
--- vector_test.go
+++ vector_test.go
@@ -6,15 +6,45 @@ import (
 )
 
 const (
-	firstArraySize  = 256 * 1024
+	firstArraySize  = 128 * 1024
 	secondArraySize = 1024 * 1024
-	maxArrayValue   = 1 * 1024 * 1024
+	maxArrayValue   = 16 * 1024 * 1024
 )
 
 var (
 	firstArrayInt  = randArray(firstArraySize, maxArrayValue)
 	secondArrayInt = randArray(secondArraySize, maxArrayValue)
 )
+
+func TestVectorAdd(t *testing.T) {
+	vec := NewVector()
+	for _, n := range firstArrayInt[:256] {
+		vec.Add(n)
+	}
+	it := NewVectorIter(vec)
+	for {
+		base, data, ok := it.Next()
+		if !ok {
+			break
+		}
+		t.Log(base, data)
+	}
+}
+
+func TestVector2Add(t *testing.T) {
+	vec := NewVector2()
+	for _, n := range firstArrayInt[:256] {
+		vec.Add(n)
+	}
+	it := NewVector2Iter(vec)
+	for {
+		base, mask, data, ok := it.Next()
+		if !ok {
+			break
+		}
+		t.Log(base, mask, data)
+	}
+}
 
 func TestVectorDump(t *testing.T) {
 	vec := NewVector()
