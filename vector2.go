984ca84121a04a65d3cc4933c8ec59216a00ca49
freepk <freepk@mail.ru>
2022-10-18 12:01:14 +0300

    Broadcast

--- vector2.go
+++ vector2.go
@@ -65,15 +65,6 @@ func (v *Vector2) Bytes() []uint8 {
 	return p[:n]
 }
 
-func (v *Vector2) unpack(data *[4]uint64, pos int) (base uint16, next int) {
-	var size, mask uint8
-	_ = mask
-	base, size, mask = decode2(v.data[pos])
-	*data = *(*[4]uint64)(unsafe.Pointer(&v.data[(pos + 1)]))
-	next = pos + 1 + int(size) + 1
-	return
-}
-
 type Vector2Iter struct {
 	pos int
 	vec *Vector2
@@ -97,11 +88,65 @@ func (vi *Vector2Iter) hasNext() bool {
 	return true
 }
 
-func (vi *Vector2Iter) Next() (base uint16, data [4]uint64, ok bool) {
+func (vi *Vector2Iter) Next() (base uint16, data [8]uint32, ok bool) {
 	if !vi.hasNext() {
 		return
 	}
-	base, vi.pos = vi.vec.unpack(&data, vi.pos)
+	var mask uint8
+	base, _, mask = decode2(vi.vec.data[vi.pos])
+	vi.pos++
+	switch mask & 0b00000011 {
+	case 3:
+		data[0] = vi.vec.data[vi.pos]
+		vi.pos++
+		data[1] = vi.vec.data[vi.pos]
+		vi.pos++
+	case 2:
+		data[0] = vi.vec.data[vi.pos]
+		vi.pos++
+	case 1:
+		data[1] = vi.vec.data[vi.pos]
+		vi.pos++
+	}
+	switch mask & 0b00001100 {
+	case (3 << 2):
+		data[2] = vi.vec.data[vi.pos]
+		vi.pos++
+		data[3] = vi.vec.data[vi.pos]
+		vi.pos++
+	case (2 << 2):
+		data[2] = vi.vec.data[vi.pos]
+		vi.pos++
+	case (1 << 2):
+		data[3] = vi.vec.data[vi.pos]
+		vi.pos++
+	}
+	switch mask & 0b00110000 {
+	case (3 << 4):
+		data[4] = vi.vec.data[vi.pos]
+		vi.pos++
+		data[5] = vi.vec.data[vi.pos]
+		vi.pos++
+	case (2 << 4):
+		data[4] = vi.vec.data[vi.pos]
+		vi.pos++
+	case (1 << 4):
+		data[5] = vi.vec.data[vi.pos]
+		vi.pos++
+	}
+	switch mask & 0b11000000 {
+	case (3 << 6):
+		data[6] = vi.vec.data[vi.pos]
+		vi.pos++
+		data[7] = vi.vec.data[vi.pos]
+		vi.pos++
+	case (2 << 6):
+		data[6] = vi.vec.data[vi.pos]
+		vi.pos++
+	case (1 << 6):
+		data[7] = vi.vec.data[vi.pos]
+		vi.pos++
+	}
 	ok = true
 	return
 }
